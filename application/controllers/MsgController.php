67e11b93357e387951e4977b9b43d5f2449adc12
zhangchile <zzlchile@gmail.com>
2014-02-17 16:14:13 +0800

    add new files.

---
+++ application/controllers/MsgController.php
@@ -0,0 +1,32 @@
+<?php
+
+class MsgController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function successAction()
+    {
+        // action body
+    }
+
+    public function failAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
