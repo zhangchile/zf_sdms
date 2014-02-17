a8e117c224edd38b95cbcbbbcbc07a6a9cdc3033
zhangchile <zzlchile@gmail.com>
2014-02-17 13:45:28 +0800

    update controllers home~,student~.

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -12,8 +12,6 @@ class HomeController extends Zend_Controller_Action
         // action body
         $total = new Application_Model_DbTable_Total();
         $this->view->info = $total->getTotal();
-        $session = new Zend_Session_Namespace();
-        $this->view->name = $session->login['name'];
     }
 
 
--- application/controllers/StudentController.php
+++ application/controllers/StudentController.php
@@ -11,6 +11,8 @@ class StudentController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $students = new Application_Model_DbTable_Normal();
+        $this->view->result = $students->get_info('student');
     }
 
 
